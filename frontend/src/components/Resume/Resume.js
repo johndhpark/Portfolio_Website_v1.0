45e5a097ef8701c31b7f26c49ca8539bfd379c31
John DH. Park <john.dh.park@me.com>
2019-09-15 00:23:53 -0700

    Removed the Container element around the embed element in Resume component

--- frontend/src/components/Resume/Resume.js
+++ frontend/src/components/Resume/Resume.js
@@ -3,27 +3,21 @@ import styled from "styled-components";
 import resume from "../../assets/images/John_DH_Park_Resume_Frontend_Engineer.pdf";
 
 const Container = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
   height: 100%;
   width: 100%;
 `;
 
 const Viewer = styled.embed`
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
 `;
 
 const Resume = props => {
   return (
-    <Container>
-      <Viewer
-        src={resume}
-        type="application/pdf"
-        title="John DH. Park Resume"
-      />
-    </Container>
+    <Viewer src={resume} type="application/pdf" title="John DH. Park Resume" />
   );
 };
 
