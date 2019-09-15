ff020ab2e804c19ecadb9d47ad43d3d0f910e8a6
John DH. Park <john.dh.park@me.com>
2019-09-15 01:12:35 -0700

    Adjustment of padding for pdf viewer

--- frontend/src/components/Resume/Resume.js
+++ frontend/src/components/Resume/Resume.js
@@ -2,32 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import resume from "../../assets/images/John_DH_Park_Resume_Frontend_Engineer.pdf";
 
-const Container = styled.div`
-  position: absolute;
-  height: 100%;
-  width: 100%;
-  padding: 5px;
-  background-color: gray;
-`;
-
 const Viewer = styled.embed`
-  position: relative;
+  position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  padding: 10px;
 `;
 
 const Resume = props => {
   return (
-    <Container>
-      <Viewer
-        src={resume}
-        type="application/pdf"
-        title="John DH. Park Resume"
-      />
-    </Container>
+    <Viewer src={resume} type="application/pdf" title="John DH. Park Resume" />
   );
 };
 
