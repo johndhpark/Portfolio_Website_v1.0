9523c1f7f3e46ca9423774341f965f22635afcb1
John DH. Park <john.dh.park@me.com>
2019-09-14 23:54:00 -0700

    Removed unnecessary import in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
-import Resume from "./components/Resume/Resume";
 
 const Resume = React.lazy(() => {
   return import("./components/Resume/Resume");
